907d9ad8bd3fe2f1ee9bcebeda600192d95f9e01
User <User@192.168.0.10>
2016-12-23 15:07:03 +0800

    Modify the message

--- src/travistest4/Test3.java
+++ src/travistest4/Test3.java
@@ -15,7 +15,7 @@ public class Test3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.print("LOlo");
+        System.out.print("UPdate thins");
     }
     
 }
